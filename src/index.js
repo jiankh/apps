36f95945a9ab6396baddfc3e5505b44ebda4d7cd
jiankh <131821436+jiankh@users.noreply.github.com>
2023-10-26 22:43:56 -0400

    added more moving css to content

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ function createProject(img, title, description, codeLink, previewLink) {
     project.classList.add("project")
     project.innerHTML = `
     <div class="project-img"><img src="${img}"></div>
+    <hr>
     <div class="project-description">
         <div class="project-header">
             <div class="project-title">${title}</div>
             <div class="buttons">
-                <input type="button" onclick="location.href='${codeLink}';" value="View Code" />
-                <input type="button" onclick="location.href='${previewLink}';" value="Live Preview" />
+                <button onclick="location.href='${codeLink}';" ><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" /> </button>
+                <button onclick="location.href='${previewLink}';"> <img src="/dist/images/open-in-new.svg"> </button>
             </div>
         </div>
         <div class="description"> ${description} </div>
@@ -29,4 +30,20 @@ Projects.forEach((project) => {
 const disabledButtons = document.querySelectorAll('input[type="button"][onclick="location.href=\'\';"]');
 disabledButtons.forEach((button)=> {
     button.disabled = true
+})
+
+//Scrolling
+window.addEventListener('scroll', function() {
+    const desiredScrollPosition = 500; // Change this value to the desired scroll position
+    const projectContainer = document.querySelector(".projects-container");
+    const project = document.querySelectorAll(".project")
+
+    if (window.scrollY >= desiredScrollPosition) {
+        projectContainer.classList.add('scroll-to-color');
+        project.forEach(proj => proj.classList.add("scroll-move"))
+        
+    } else {
+        projectContainer.classList.remove('scroll-to-color');
+        project.forEach(proj => proj.classList.remove("scroll-move"))
+    }
 })
